74176b9913fd31c00ad4c04cc9f0d45199e16d1e
GigaGrunch <gigagrunch@gmail.com>
2020-11-29 14:21:11 +0100

    test verifies chat message

--- GameLibrary/Game.cs
+++ GameLibrary/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
@@ -7,27 +8,67 @@ namespace DiskWars
 {
     public struct Game
     {
-        public Action<string> Log;
+        public delegate void LogCallback(string message);
+        public LogCallback Log;
 
-        public NetworkStream networkStream;
+        public delegate void ChatCallback(string message);
+        public ChatCallback ChatReceived;
 
-        public async void HostServer(int port)
+        public StreamWriter networkWriter;
+        public StreamReader networkReader;
+
+        public async Task HostServer(int port)
         {
             Log("creating TCP listener");
             TcpListener listener = new TcpListener(IPAddress.Any, port: port);
             listener.Start();
+
             TcpClient client = await listener.AcceptTcpClientAsync();
             Log("a client connected");
-            networkStream = client.GetStream();
+            NetworkStream networkStream = client.GetStream();
+
+            networkWriter = new StreamWriter(networkStream);
+            networkReader = new StreamReader(networkStream);
+
+            HandleIncomingMessages();
+            SendMessage("hello, I am the host!");
         }
 
-        public async void ConnectClient(string host, int port)
+        public async Task ConnectClient(string host, int port)
         {
             Log("creating TCP client");
             TcpClient tcpClient = new TcpClient();
+
             await tcpClient.ConnectAsync(host: host, port: port);
             Log("connected to server");
-            networkStream = tcpClient.GetStream();
+            NetworkStream networkStream = tcpClient.GetStream();
+
+            networkWriter = new StreamWriter(networkStream);
+            networkReader = new StreamReader(networkStream);
+
+            HandleIncomingMessages();
+            SendMessage("hello, I am a client!");
+        }
+
+        public void SendMessage(string message)
+        {
+            networkWriter.WriteLine(message);
+            networkWriter.Flush();
+        }
+
+        public async void HandleIncomingMessages()
+        {
+            string message = null;
+            async Task<bool> ReadNext(StreamReader reader)
+            {
+                message = await reader.ReadLineAsync();
+                return message != null;
+            }
+
+            while (await ReadNext(networkReader))
+            {
+                ChatReceived(message);
+            }
         }
     }
 }
--- Tests/GameTests.cs
+++ Tests/GameTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -6,20 +8,41 @@ namespace DiskWars.Tests
 {
     public class GameTests
     {
-        [Fact]
-        public void StartHost_StartClient_Connect()
+        [Fact(Timeout = 1000)]
+        public async void StartHost_StartClient_Connect()
         {
+            List<string> hostChats = new List<string>();
+            List<string> clientChats = new List<string>();
+
             Game host = new Game
             {
-                Log = message => Console.WriteLine($"HOST: {message}")
+                Log = message => Console.WriteLine($"HOST: {message}"),
+                ChatReceived = message => hostChats.Add(message)
             };
             Game client = new Game
             {
-                Log = message => Console.WriteLine($"CLIENT: {message}")
+                Log = message => Console.WriteLine($"CLIENT: {message}"),
+                ChatReceived = message => clientChats.Add(message)
             };
-            
-            host.HostServer(port: 7777);
-            client.ConnectClient(host: "localhost", port: 7777);
+
+            Task.WaitAll(
+                host.HostServer(port: 7777),
+                client.ConnectClient(host: "localhost", port: 7777));
+
+            await WaitForChat();
+
+            async Task WaitForChat()
+            {
+                while (true)
+                {
+                    if (hostChats.Count > 0 && clientChats.Count > 0)
+                    {
+                        return;
+                    }
+
+                    await Task.Delay(10);
+                }
+            }
         }
     }
 }
